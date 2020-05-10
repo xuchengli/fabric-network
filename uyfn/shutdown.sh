1e6eec324a8b4971a2fff9c713d3ec29dcc55a28
xuchengli <lixucheng@aliyun.com>
2020-05-10 21:00:30 +0800

    add extra orderer artifacts

--- uyfn/shutdown.sh
+++ uyfn/shutdown.sh
@@ -28,7 +28,7 @@ function networkDown() {
   #Cleanup images
   removeUnwantedImages
   # remove orderer block and other channel configuration transactions and certs
-  rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+  rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config ./extra-orderer-artifacts/crypto-config/ channel-artifacts/orderer.json
 }
 
 networkDown
